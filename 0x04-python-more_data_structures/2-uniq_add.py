81ffdb7989697e54736a45498f990523dba9a2de
root <root@Flex.localdomain>
2022-12-08 21:16:15 +0300

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+
 def uniq_add(my_list=[]):
-    uniq_list = set(my_list)
-    num = 0
+    """Add all unique integers in a list (once for each integer)."""
+    result = 0
+    for x in set(my_list):
+        result += x
+        return (result)
 
-    for i in uniq_list
-    num += 1
-
-    return (num)
--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,9 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
+
 def number_keys(a_dictionary):
-    num = 0
-    list_keys = list(a_dictionary.keys())
-
-    for i in list_keys:
-        num += 1
-
-        return (num)
+    """Return the number of keys in a dictionary."""
+    return (len(a_dictionary))
