e228217e813f571e3265342e9cc431f92fd3a7d6
Felix-Muraguri <root@Flex>
2022-12-19 12:07:30 +0300

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for a in range(ord('a'), ord('z') + 1):
-    if chr(a) != 'e'and chr(a) != 'q':
-        print("{:c}".format(a), end='')
+for a in range(97, 123):
+    if chr(i) != 'e'and chr(i) != 'q':
+        print("{}".format(i)), end='')
