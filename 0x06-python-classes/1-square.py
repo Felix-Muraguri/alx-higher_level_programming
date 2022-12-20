cef5892e046a3963a885121bd9ce9cffa3195e3e
Felix-Muraguri <root@Flex>
2022-12-20 10:35:40 +0300

    100-singly_linked_list.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""A class Square that defines a square (based on 0-square.py)"""
+
+class Square: 
+    """Square class with a private attribute - size."""
+
+    def __init__(self, size):
+        """Initializes the size variable as a private instance attribute"""
+        self.__size = size
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A class Square that defines a square based on 3-square.py"""
+
+class Square:
+    """Square class with a private attribute - size."""
+
+    def __init__(self, size = 0):
+        """Initializes the size variable as a private instance attribute."""
+        self.__size = size
+
+        @property
+        def size(self):
+            """Instantiation with optional size of square."""
+            return self.__size
+
+        @size.setter
+        def size(self, size_value):
+            """Gets the size of the square"""
+            self.__size = size_value
+
+            if not isinstance(size_value,  int):
+                raise TypeError("size must be an integer")
+            elif size_value < 0:
+                raise ValueError("size must be >= 0")
+
+        def area(self):
+            """Returns the current square area"""
+            return self.__dize ** 2
