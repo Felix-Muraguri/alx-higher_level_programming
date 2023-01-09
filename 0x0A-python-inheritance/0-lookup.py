598c05399398760dcfbc53f11260543c8e8e93c4
Felix-Muraguri <root@Flex>
2023-01-09 18:16:39 +0300

    0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Author: Felix"""
+
+def lookup(obj):
+    """A funtion that returns the list of available
+    attributes and methods of an object
+    Returns a list object
+
+    """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Geometry module.
+Author: Felix
+
+"""
+
+
+class BaseGeometry:
+    """An empty class"""
+    pass
